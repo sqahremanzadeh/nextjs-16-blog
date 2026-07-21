866453dd1b34b78411a9fc6bc8d3a268df592d4c
Saeed <44677558+sqahremanzadeh@users.noreply.github.com>
2026-07-22 01:02:55 +0330

    import fixed

--- app/(shared-layout)/blogs/page.tsx
+++ app/(shared-layout)/blogs/page.tsx
@@ -7,6 +7,7 @@ import { Metadata } from "next";
 import { cacheLife, cacheTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
+import { connection } from "next/server";
 import { Suspense } from "react";
 
 // export const dynamic = "force-static";
